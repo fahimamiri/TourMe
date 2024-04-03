b9d6e76b480fd9b6686023d284e21bdf3bcbee4a
fahim amiri <famiri1@mail.sfsu.eduu>
2024-04-02 18:49:42 -0700

    filesadded

---
+++ tourme123/tourme123/tourme123App.swift
@@ -0,0 +1,17 @@
+//
+//  tourme123App.swift
+//  tourme123
+//
+//  Created by Fahim Amiri on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct tourme123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
